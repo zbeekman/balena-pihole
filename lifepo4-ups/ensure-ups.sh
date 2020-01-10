01bbd25bb75ab44e36a5d43fd41f54ecc6a2c9f0
Izaak Beekman <zbeekman@gmail.com>
2020-01-10 17:29:45 -0500

    Try different approach to service

--- lifepo4-ups/ensure-ups.sh
+++ lifepo4-ups/ensure-ups.sh
@@ -1,8 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env/ bash
+# Don't run the daemon in the background because the shell will exit.
 
+service lifepo4wered-daemon status
 update-rc.d lifepo4wered-daemon defaults
-service lifepo4wered-daemon restart
 lifepo4wered-cli set auto_boot 1 # Auto-boot whenever there is power to do so
 lifepo4wered-cli set cfg_write 0x46 # Save this setting
 echo "Current battery voltage in mV:"
 lifepo4wered-cli get vbat
+#service lifepo4wered-daemon restart
+bash -x /etc/init.d/lifepo4wered-daemon restart
