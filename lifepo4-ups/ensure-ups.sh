5108b18648faef6bc6cd6b292f6d45ed6c73dc7f
Izaak Beekman <zbeekman@gmail.com>
2020-01-10 17:59:45 -0500

    Ensure I2C-dev module loaded and better UPS output

--- lifepo4-ups/ensure-ups.sh
+++ lifepo4-ups/ensure-ups.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
-# Don't run the daemon in the background because the shell will exit.
+# Run the daemon in the background but enter an infinite loop to report the status
 
 update-rc.d lifepo4wered-daemon defaults
 service lifepo4wered-daemon status
 
 lifepo4wered-cli set auto_boot 1 # Auto-boot whenever there is power to do so
 lifepo4wered-cli set cfg_write 0x46 # Save this setting
+echo "All UPS settings:"
+lifepo4wered-cli get
 echo "Current battery voltage in mV:"
 lifepo4wered-cli get vbat
 service lifepo4wered-daemon status
@@ -14,7 +16,8 @@ service lifepo4wered-daemon restart
 service lifepo4wered-daemon status
 
 while true ; do
-    sleep 600
+    sleep "${LIFEPO4_REPORT_INTERVAL:-600}"
+    echo "Current battery voltage in mV:"
     lifepo4wered-cli get vbat
     service lifepo4wered-daemon status
 done
