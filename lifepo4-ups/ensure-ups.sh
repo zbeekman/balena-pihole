b3c4e11aef9a1ee240645c6471e961b6ce75c23b
Izaak Beekman <zbeekman@gmail.com>
2020-01-10 17:43:21 -0500

    Use and infinite loop to try to prevent service thrashing

--- lifepo4-ups/ensure-ups.sh
+++ lifepo4-ups/ensure-ups.sh
@@ -2,13 +2,19 @@
 # Don't run the daemon in the background because the shell will exit.
 
 update-rc.d lifepo4wered-daemon defaults
-service lifepo4wered-daemon start
+service lifepo4wered-daemon status
 
 lifepo4wered-cli set auto_boot 1 # Auto-boot whenever there is power to do so
 lifepo4wered-cli set cfg_write 0x46 # Save this setting
 echo "Current battery voltage in mV:"
 lifepo4wered-cli get vbat
 service lifepo4wered-daemon status
-#service lifepo4wered-daemon restart
-bash -x /etc/init.d/lifepo4wered-daemon restart
+service lifepo4wered-daemon restart
+#bash -x /etc/init.d/lifepo4wered-daemon restart
 service lifepo4wered-daemon status
+
+while true ; do
+    sleep 600
+    lifepo4wered-cli get vbat
+    service lifepo4wered-daemon status
+done
